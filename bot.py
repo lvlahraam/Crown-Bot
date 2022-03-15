4a0e3fa9b215205cd2b2f742892b56d7589a20b6
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2022-03-15 21:55:10 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 # NOTE: THIS BOT DOES NOT DOWNLOAD ANY MUSIC ON THE DIRECTORY
 
-import logging, os, pathlib, deezer
-import deezloader.deezloader as deezloader
+import logging, os, pathlib, time, deezer
+from deezloader import deezloader
 from telegram import Update, BotCommand, InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent, InputMediaPhoto
 from telegram.ext import Updater, Filters, CommandHandler, MessageHandler, CallbackQueryHandler, InlineQueryHandler, CallbackContext
 
@@ -11,7 +11,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 dezclient = deezer.Client()
-dezloader = deezloader.DeeLogin(email=os.getenv("EMAIL"), password=os.getenv("PASSWORD"))
+dezloader = deezloader.DeeLogin(arl=os.getenv("ARL"))
 
 def start(update: Update, context: CallbackContext):
     keyboard = [
@@ -38,54 +38,47 @@ def help(update: Update, context: CallbackContext):
 def searching(update: Update, context: CallbackContext):
     text = update.message.text
     if "/" in text:
-        text = text.split("/")
-        if "deezer" not in text[2]:
-            keyboard = [
-	    	    [InlineKeyboardButton("Search Artist 👤", switch_inline_query_current_chat=".art ")],
-	    	    [
-			        InlineKeyboardButton("Search Album 📀", switch_inline_query_current_chat=".alb "),
-		    	    InlineKeyboardButton("Search Track 📼", switch_inline_query_current_chat=".trk ")
-	    	    ]
-	        ]
-            markup = InlineKeyboardMarkup(keyboard)
-            update.message.reply_text(text="The URL needs to be from Deezer!", reply_markup=markup)
-        elif text[3] not in ("artist", "album"):
-            keyboard = [
-	    	    [InlineKeyboardButton("Search Artist 👤", switch_inline_query_current_chat=".art ")],
-	    	    [
-		            InlineKeyboardButton("Search Album 📀", switch_inline_query_current_chat=".alb "),
-		            InlineKeyboardButton("Search Track 📼", switch_inline_query_current_chat=".trk ")
-	    	    ]
-	        ]
-            markup = InlineKeyboardMarkup(keyboard)
-            update.message.reply_text(text="Invalid Deezer URL!", reply_markup=markup)
-        elif text[3] == "artist":
-            artist = dezclient.get_artist(text[4])
-            keyboard = [
-                [
-                    InlineKeyboardButton("Top 10 Tracks 🌟", callback_data=F"top10|{artist.id}"),
-                    InlineKeyboardButton("Albums 📼", callback_data=F"albums|{artist.id}")
+        items = text.split("/")
+        if "deezer" in items[2]:
+            if items[3] not in ("artist", "album"):
+                keyboard = [
+                    [InlineKeyboardButton("Search Artist 👤", switch_inline_query_current_chat=".art ")],
+                    [
+                        InlineKeyboardButton("Search Album 📀", switch_inline_query_current_chat=".alb "),
+                        InlineKeyboardButton("Search Track 📼", switch_inline_query_current_chat=".trk ")
+                    ]
                 ]
-            ]
-            markup = InlineKeyboardMarkup(keyboard)
-            update.message.reply_photo(photo=artist.picture_big, caption=F"{artist.name}\n{artist.nb_album}\n{artist.nb_fan}", reply_markup=markup)
-        elif text[3] == "album":
-            album = dezclient.get_album(text[4])
-            tracks = album.get_tracks()
-            keyboard = []
-            if len(tracks) > 1:
-                counter = 1
-                for track in tracks:
-                    key = [InlineKeyboardButton(F"{counter}. {track.title} 📀", callback_data=F"download|{track.id}")]
+                markup = InlineKeyboardMarkup(keyboard)
+                update.message.reply_text(text="Invalid Deezer URL!", reply_markup=markup)
+            elif items[3] == "artist":
+                artist = dezclient.get_artist(items[4])
+                keyboard = [
+                    [
+                        InlineKeyboardButton("Top 10 Tracks 🌟", callback_data=F"top10|{artist.id}"),
+                        InlineKeyboardButton("Albums 📼", switch_inline_query_current_chat=F".albs {artist.id}")
+                    ]
+                ]
+                markup = InlineKeyboardMarkup(keyboard)
+                update.message.reply_photo(photo=artist.picture_big, caption=F"{artist.name}\n{artist.nb_album}\n{artist.nb_fan}", reply_markup=markup)
+            elif items[3] == "album":
+                album = dezclient.get_album(items[4])
+                tracks = album.get_tracks()
+                keyboard = []
+                if len(tracks) > 1:
+                    counter = 1
+                    for track in tracks:
+                        key = [InlineKeyboardButton(F"{counter}. {track.title} 📀", callback_data=F"download|{track.id}")]
+                        keyboard.append(key)
+                        counter += 1
+                else:
+                    key = [InlineKeyboardButton(F"{tracks[0].title} 📀", callback_data=F"download|{tracks[0].id}")]
                     keyboard.append(key)
-                    counter += 1
-            else:
-                key = [InlineKeyboardButton(F"{tracks[0].title} 📀", callback_data=F"download|{tracks[0].id}")]
-                keyboard.append(key)
-            if len(tracks) > 1: keyboard.append([InlineKeyboardButton(F"Get All Tracks 💣", callback_data=F"getall|{album.id}")])
-            keyboard.append([InlineKeyboardButton(F"Go To Artist 👤", callback_data=F"goartist|{album.artist.id}")])
-            markup = InlineKeyboardMarkup(keyboard)
-            update.message.reply_photo(photo=album.cover_big, caption=F"{album.artist.name} - {album.title}", reply_markup=markup)
+                if len(tracks) > 1: keyboard.append([InlineKeyboardButton(F"Get All Tracks 💣", callback_data=F"getall|{album.id}")])
+                keyboard.append([InlineKeyboardButton(F"Go To Artist 👤", callback_data=F"goartist|{album.artist.id}")])
+                markup = InlineKeyboardMarkup(keyboard)
+                update.message.reply_photo(photo=album.cover_big, caption=F"{album.artist.name} - {album.title}", reply_markup=markup)
+        else:
+            update.message.reply_text(text=F"Invalid {'spotify' if 'spotify'in items[2] else 'deezer'} url!\n\n1. The url must either be from deezer.com or spotify.com\n2. If your given url is a playlist:\nThis Bot will not download playlists, Due to the playlist might be a day long")
     else:
         keyboard = [
             [InlineKeyboardButton("Search Artist 👤", switch_inline_query_current_chat=F".art {text}")],
@@ -141,7 +134,7 @@ def button(update: Update, context: CallbackContext):
         keyboard = [
             [
                 InlineKeyboardButton("Top 10 Tracks 🌟", callback_data=F"top10|{artist.id}"),
-                InlineKeyboardButton("Albums 📼", callback_data=F"albums|{artist.id}")
+                InlineKeyboardButton("Albums 📼", switch_inline_query_current_chat=F".albs {artist.id}")
             ]
         ]
         markup = InlineKeyboardMarkup(keyboard)
@@ -182,7 +175,7 @@ def button(update: Update, context: CallbackContext):
                 method_save=1
             )
             query.answer(F"Downloaded {track.title} track...")
-            query.message.reply_audio(audio=pathlib.Path(download.song_path).read_bytes(), filename=F"{track.artist} - {track.title}", title=track.title, performer=track.artist.name, duration=track.duration, thumb=track.album.cover_big)
+            query.message.reply_audio(audio=pathlib.Path(download.song_path).read_bytes(), filename=F"{track.artist} - {track.title}", title=track.title, performer=track.artist.name, duration=track.duration, thumb=track.album.cover_big, timeout=60)
         query.message.reply_text("Done!")
     elif relate == "download":
         track = dezclient.get_track(id)
@@ -195,12 +188,15 @@ def button(update: Update, context: CallbackContext):
             recursive_download=True,
             method_save=1
         )
-        query.message.reply_audio(audio=pathlib.Path(download.song_path).read_bytes(), filename=F"{track.artist} - {track.title}", title=track.title, performer=track.artist.name, duration=track.duration, thumb=track.album.cover_big)
+        query.message.reply_audio(audio=pathlib.Path(download.song_path).read_bytes(), filename=F"{track.artist} - {track.title}", title=track.title, performer=track.artist.name, duration=track.duration, thumb=track.album.cover_big, timeout=60)
 
 def inline(update: Update, context: CallbackContext):
     text = update.inline_query.query
-    if not text or not text.startswith(".") or text == ".art" or text == ".art " or text == ".alb" or text == ".alb " or text == ".trk" or text == ".trk ": return
-    if text.startswith(".art"):
+    if not text or not text.startswith(".") or not text.startswith(".a") or not text.startswith(".t") or text == ".albs" or text == ".albs " or text == ".art" or text == ".art " or text == ".alb" or text == ".alb " or text == ".trk" or text == ".trk": return
+    if text.startswith(".albs"):
+        text = text.strip(".albs ")
+        search = dezclient.get_artist(text).get_albums()
+    elif text.startswith(".art"):
         text = text.strip(".art ")
         search = dezclient.search_artists(query=text)
     elif text.startswith(".alb"):
@@ -218,7 +214,7 @@ def inline(update: Update, context: CallbackContext):
             thumbnail = data.picture
         elif isinstance(data, deezer.Album):
             title = data.title
-            description = F"Artist: {data.artist.name}\nTracks: {data.nb_tracks}"
+            description = F"Artist: {data.artist.name}\nTracks: {data.nb_tracks or 1}"
             thumbnail = data.cover
         elif isinstance(data, deezer.Track):
             title = data.title
@@ -243,7 +239,7 @@ def error(update: Update, context: CallbackContext):
 
 def main():
     try:
-        updater = Updater(os.getenv("TOKEN"))
+        updater = Updater(token=os.getenv("TOKEN"))
 
         print(F"Hello I'm: {updater.bot.name}")
 
