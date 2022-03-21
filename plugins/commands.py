2ecc4236668508dfe550545e3f0ced5a49cb17af
lvlahraam <lvlahraam@gmail.com>
2022-03-21 22:29:06 +0330

    .

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,15 +2,7 @@ import pyrogram
 
 @pyrogram.Client.on_message(pyrogram.filters.command("start"))
 async def start(client:pyrogram.Client, message:pyrogram.types.Message):
-    keyboard = [
-        [pyrogram.types.InlineKeyboardButton(text="Search Artist 👤", switch_inline_query_current_chat=".art ")],
-        [
-            pyrogram.types.InlineKeyboardButton(text="Search Album 📼", switch_inline_query_current_chat=".alb "),
-            pyrogram.types.InlineKeyboardButton(text="Search Track 💿", switch_inline_query_current_chat=".trk ")
-        ]
-    ]
-    markup = pyrogram.types.InlineKeyboardMarkup(keyboard)
-    await message.reply_text(text="Hi\nI'm Crown", reply_markup=markup)
+    await message.reply_text(text="Hi, I'm Crown, I can help you find the music you want\nTry my /help command to see what commands I've got")
 
 @pyrogram.Client.on_message(pyrogram.filters.command("help"))
 async def help(client:pyrogram.Client, message:pyrogram.types.Message):
@@ -22,7 +14,7 @@ async def help(client:pyrogram.Client, message:pyrogram.types.Message):
         ]
     ]
     markup = pyrogram.types.InlineKeyboardMarkup(keyboard)
-    await message.reply_text(text="You can search for music by sending a message\nOr sending the deezer url\nOr by mentioning me in the chat and using:\n.art (as artist) - .alb (as album) - .trk (as track) and typing the query you want in front of it\nFor example: `@crownmusicbot .alb Dawn Fm`\n\nOr even by using these buttons bellow\n\nFor artist's albums you could do\nFor example: `@crownmusicbot .albs [artist's deezer id]`\nFor artist's tracks you could do\nFor example: `@crownmusicbot .trks [artist's deezer id]\nOr even by just searching for the artist and then click on albums or tracks button", reply_markup=markup)
+    await message.reply_text(text="You can search for music by sending a message\nOr sending the deezer url\nOr by mentioning me in the chat and using:\n.art (as artist) - .alb (as album) - .trk (as track) and typing the query you want in front of it\nFor example: `@crownmusicbot .alb Dawn Fm`\n\nOr even by using these buttons bellow\n\nFor artist's albums you could do\nFor example: `@crownmusicbot .albs [artist's deezer id]`\nFor artist's tracks you could do\nFor example: `@crownmusicbot .trks [artist's deezer id]\nOr even by just searching for the artist and then click on albums or tracks button\n\nIf you wanted to see some information about yourself\nYou could try the /me command", reply_markup=markup)
 
 @pyrogram.Client.on_message(pyrogram.filters.command("me"))
 async def me(client:pyrogram.Client, message:pyrogram.types.Message):
--- plugins/message.py
+++ plugins/message.py
@@ -3,17 +3,17 @@ import pyrogram
 @pyrogram.Client.on_message(pyrogram.filters.private | pyrogram.filters.regex("spotify"))
 async def search(client:pyrogram.Client, message:pyrogram.types.Message):
     text = message.text
-    if "/" in text:
-        items = text.split("/")
-        site = items[2]
-        relate = items[3]
-        id = items[4]
-    elif ":" in text:
-        items = text.split(":")
-        site = items[0]
-        relate = items[1]
-        id = items[2]
-    if "spotify" in site:
+    if "spotify" in text:
+        if "/" in text:
+            items = text.split("/")
+            relate = items[3]
+            id = items[4]
+        elif ":" in text:
+            items = text.split(":")
+            relate = items[1]
+            id = items[2]
+        else:
+            await message.reply_text(text=F"Invalid Spotify URL!\n\n1. The url must be from spotify.com\n2. If your given url is a playlist:\nThis Bot will not download playlists, Due to the playlist might be a day long")
         if relate not in ("artist", "album", "track"):
             keyboard = [
                 [pyrogram.types.InlineKeyboardButton(text="Search Artist 👤", switch_inline_query_current_chat=".art ")],
@@ -23,7 +23,7 @@ async def search(client:pyrogram.Client, message:pyrogram.types.Message):
                 ]
             ]
             markup = pyrogram.types.InlineKeyboardMarkup(keyboard)
-            return await message.reply_text(text="Invalid Deezer URL!", reply_markup=markup)
+            await message.reply_text(text="Invalid Deezer URL!", reply_markup=markup)
         elif relate == "artist":
             artist = client.spoitfy.artist(id)
             keyboard = [
@@ -33,7 +33,7 @@ async def search(client:pyrogram.Client, message:pyrogram.types.Message):
                 ]
             ]
             markup = pyrogram.types.InlineKeyboardMarkup(keyboard)
-            return await message.reply_photo(photo=artist["images"][0]["url"], caption=artist["name"], reply_markup=markup)
+            await message.reply_photo(photo=artist["images"][0]["url"], caption=artist["name"], reply_markup=markup)
         elif relate == "album":
             album = client.spotify.album(id)
             tracks = album['tracks']['items']
@@ -50,7 +50,7 @@ async def search(client:pyrogram.Client, message:pyrogram.types.Message):
             if len(tracks) > 1: keyboard.append([pyrogram.types.InlineKeyboardButton(F"Get all Tracks 💽", callback_data=F"getall|{album['id']}")])
             keyboard.append([pyrogram.types.InlineKeyboardButton(F"Go to Artist 👤", callback_data=F"goartist|{album['artists'][0]['id']}")])
             markup = pyrogram.types.InlineKeyboardMarkup(keyboard)
-            return await message.reply_photo(photo=album['images'][0]['url'], caption=F"{album['artists'][0]['name']} - {album['name']}", reply_markup=markup)
+            await message.reply_photo(photo=album['images'][0]['url'], caption=F"{album['artists'][0]['name']} - {album['name']}", reply_markup=markup)
         elif relate == "track":
             track = client.spotify.track(id)
             keyboard = [
@@ -64,15 +64,14 @@ async def search(client:pyrogram.Client, message:pyrogram.types.Message):
                 ]
             ]
             markup = pyrogram.types.InlineKeyboardMarkup(keyboard)
-            return await message.reply_photo(photo=track['album']['images'][0]['url'], caption=F"{track['artists'][0]['name']} - {track['name']}", reply_markup=markup)
-    elif "spotify" not in site:
-        return await message.reply_text(text=F"Invalid Spotify URL!\n\n1. The url must be from spotify.com\n2. If your given url is a playlist:\nThis Bot will not download playlists, Due to the playlist might be a day long")
-    keyboard = [
-        [pyrogram.types.InlineKeyboardButton(text="Search Artist 👤", switch_inline_query_current_chat=F".art {text}")],
-        [
-            pyrogram.types.InlineKeyboardButton(text="Search Album 📼", switch_inline_query_current_chat=F".alb {text}"),
-            pyrogram.types.InlineKeyboardButton(text="Search Track 💿", switch_inline_query_current_chat=F".trk {text}")
+            await message.reply_photo(photo=track['album']['images'][0]['url'], caption=F"{track['artists'][0]['name']} - {track['name']}", reply_markup=markup)
+    else:
+        keyboard = [
+            [pyrogram.types.InlineKeyboardButton(text="Search Artist 👤", switch_inline_query_current_chat=F".art {text}")],
+            [
+                pyrogram.types.InlineKeyboardButton(text="Search Album 📼", switch_inline_query_current_chat=F".alb {text}"),
+                pyrogram.types.InlineKeyboardButton(text="Search Track 💿", switch_inline_query_current_chat=F".trk {text}")
+            ]
         ]
-    ]
-    markup = pyrogram.types.InlineKeyboardMarkup(keyboard)
-    await message.reply_text(text=F"Searched: {text}", reply_markup=markup)
+        markup = pyrogram.types.InlineKeyboardMarkup(keyboard)
+        await message.reply_text(text=F"Searched: {text}", reply_markup=markup)
