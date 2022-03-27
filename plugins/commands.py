0d9e4f543c4ccbf4c86096cd062da60c42b7e778
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2022-03-27 17:15:49 +0430

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,4 +1,4 @@
-import pyrogram
+import pyrogram, io, textwrap, contextlib, traceback
 
 @pyrogram.Client.on_message(pyrogram.filters.command("start"))
 async def start(client:pyrogram.Client, message:pyrogram.types.Message):
@@ -20,3 +20,38 @@ async def help(client:pyrogram.Client, message:pyrogram.types.Message):
 async def me(client:pyrogram.Client, message:pyrogram.types.Message):
     me = message.from_user
     await message.reply_text(text=F"ID: {me.id}\nUser Name: {me.username}\nFirst Name: {me.first_name}\nLast name: {me.last_name}\nPhone Number: {me.phone_number}\nStatus: {me.status}\nMention: {me.mention}")
+
+@pyrogram.Client.on_message(pyrogram.filters.command("eval"))
+async def eval(client:pyrogram.Client, message:pyrogram.types.Message):
+    env = {
+        "client": Client
+    }
+    body = message.command[1]
+    env.update(globals())
+    if body.startswith("```") and body.endswith("```"):
+        body = "\n".join(body.split("\n")[1:-1])
+    body = body.strip("` \n")
+    stdout = io.StringIO()
+    to_compile = F"async def func():\n{textwrap.indent(body, '  ')}"
+    try:
+        exec(to_compile, env)
+    except Exception as e:
+        return await ctx.reply(F"```py\n{e.__class__.__name__}: {e}\n```")
+    func = env["func"]
+    try:
+        with contextlib.redirect_stdout(stdout):
+            ret = await func()
+    except Exception as e:
+        value = stdout.getvalue()
+        await ctx.reply(F"```py\n{value}{traceback.format_exc()}\n```")
+    else:
+        value = stdout.getvalue()
+        try:
+            await message.add_reaction("\u2705")
+        except:
+            pass
+        if ret is None:
+            if value:
+                await message.reply_text(text=F"`\n{value}\n`")
+        else:
+            await message.reply_text(text=F"`\n{value}{ret}\n`")
