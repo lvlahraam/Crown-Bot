4b47c6b1d65952c781610331a73026ef3d06a3ee
lvlahraam <lvlahraam@gmail.com>
2022-03-29 10:37:02 +0430

    eval

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters, types, errors
+import sys, io, traceback, os
 
 @Client.on_message(filters.command("start"))
 async def start(client:Client, message:types.Message):
@@ -50,3 +51,49 @@ async def help(client:Client, message:types.Message):
     ]
     markup = types.InlineKeyboardMarkup(keyboard)
     await message.reply_text(text="You can search for music by using the /search command\nAnd putting a search query in front of it for example:\n`/search Eminem`\n\nOr by sending a Deezer URL\nOr by tagging me in the text field and using:\n.art (as artist) - .alb (as album) - .trk (as track) tags and typing the query you want in front of it for example:\n`@crownmusicbot .alb Dawn Fm`\n\nOr even by using these buttons bellow\n\nFor artist's albums you could do:\n`@crownmusicbot .albs [artist's deezer id]`\nFor artist's tracks you could do:\n `@crownmusicbot .trks [artist's deezer id]`\nOr even by just searching for the artist and then click on albums or tracks button\n\nIf you wanted to see some information about yourself\nYou could try the /me command", reply_markup=markup)
+
+@Client.on_message(filters.command("eval") & filters.user(755341301))
+async def eval(client:Client, message:types.Message):
+    cmd = message.command[1]
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+    try:
+        await aexec(cmd, client, message)
+    except Exception:
+        exc = traceback.format_exc()
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+    final_output = "<b>EVAL</b>: "
+    final_output += f"<code>{cmd}</code>\n\n"
+    final_output += "<b>OUTPUT</b>:\n"
+    final_output += f"<code>{evaluation.strip()}</code> \n"
+    if len(final_output) > 4096:
+        with io.BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.text"
+            await message.reply_document(
+                document=out_file,
+                caption=cmd[: 4096 // 4 - 1],
+                disable_notification=True,
+                quote=True,
+            )
+        os.remove("eval.text")
+    else:
+        await message.reply_text(final_output, quote=True)
+
+async def aexec(code, client, message):
+    exec("async def __aexec(client, message): " + "".join(f"\n {l_}" for l_ in code.split("\n")))
+    return await locals()["__aexec"](client, message)
